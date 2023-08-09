e1191246dad715f1bde0062e94cd8f41f5f05931
hoon2hooni <sanghoon.engineer@gmail.com>
2023-08-09 21:17:22 +0900

    :sparkles: 7-9 리펙토링

--- chapter-07/src/09-substitute-algorithm/example.ts
+++ chapter-07/src/09-substitute-algorithm/example.ts
@@ -1,18 +1,6 @@
 /**
- * 리팩토링 전
+ * 리팩토링 후
  */
 function foundPerson(people: string[]) {
-  for (let i = 0; i < people.length; i++) {
-    if (people[i] === 'Don') {
-      return 'Don';
-    }
-    if (people[i] === 'John') {
-      return 'John';
-    }
-    if (people[i] === 'Kent') {
-      return 'Kent';
-    }
-  }
-
-  return '';
+  return ['Don', 'John', 'Kent'].find((p) => people.includes(p)) || '';
 }
