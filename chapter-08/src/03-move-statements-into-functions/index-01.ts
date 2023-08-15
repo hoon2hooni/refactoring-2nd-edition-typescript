cdea5f419b44200268eae735f926e521d610f271
hoon2hooni <sanghoon.engineer@gmail.com>
2023-08-15 15:45:38 +0900

    :sparkles: 8-3 리펙토링 후

--- chapter-08/src/03-move-statements-into-functions/index-01.ts
+++ chapter-08/src/03-move-statements-into-functions/index-01.ts
@@ -12,23 +12,18 @@ function renderPerson(person: Person) {
   const result = [];
   result.push(`<p>${person.name}</p>`);
   result.push(renderPhoto(person.photo));
-  result.push(`<p>제목: ${person.photo.title}</p>`);
   result.push(emitPhotoData(person.photo));
-
   return result.join('\n');
 }
 
 function photoDiv(p: Photo) {
-  return ['<div>', `<p>제목: ${p.title}</p>`, emitPhotoData(p), '</div>'].join(
-    '\n',
-  );
+  return ['<div>', emitPhotoData(p), '</div>'].join('\n');
 }
 
-function emitPhotoData(aPhoto: Photo) {
-  const result = [];
-
-  result.push(`<p>위치: ${aPhoto.location.name}</p>`);
-  result.push(`<p>날짜: ${aPhoto.date.toDateString()}</p>`);
-
-  return result.join('\n');
+function emitPhotoData(p: Photo) {
+  return [
+    `<p>제목: ${p.title}</p>`,
+    `<p>위치: ${p.location.name}</p>`,
+    `<p>날짜: ${p.date.toDateString()}</p>`,
+  ].join('\n');
 }
