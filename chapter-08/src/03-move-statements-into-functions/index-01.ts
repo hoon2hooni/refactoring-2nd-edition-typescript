e39332e4c66df50ea2ccb8ccbf92ef1ca4194aed
hoon2hooni <sanghoon.engineer@gmail.com>
2023-08-15 15:45:11 +0900

    :tada: 8-3 시작전

--- chapter-08/src/03-move-statements-into-functions/index-01.ts
+++ chapter-08/src/03-move-statements-into-functions/index-01.ts
@@ -1,6 +1,15 @@
-function renderPerson(outStream, person) {
-  const result = [];
+declare function renderPhoto(aPhoto: Photo): string;
 
+type Person = { name: string; photo: Photo };
+type Photo = {
+  title: string;
+  date: Date;
+  location: {
+    name: string;
+  };
+};
+function renderPerson(person: Person) {
+  const result = [];
   result.push(`<p>${person.name}</p>`);
   result.push(renderPhoto(person.photo));
   result.push(`<p>제목: ${person.photo.title}</p>`);
@@ -9,13 +18,13 @@ function renderPerson(outStream, person) {
   return result.join('\n');
 }
 
-function photoDiv(p) {
+function photoDiv(p: Photo) {
   return ['<div>', `<p>제목: ${p.title}</p>`, emitPhotoData(p), '</div>'].join(
     '\n',
   );
 }
 
-function emitPhotoData(aPhoto) {
+function emitPhotoData(aPhoto: Photo) {
   const result = [];
 
   result.push(`<p>위치: ${aPhoto.location.name}</p>`);
