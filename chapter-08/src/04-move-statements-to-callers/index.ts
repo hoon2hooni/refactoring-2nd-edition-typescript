9d9104908f295fe513bf56fb85b6e774e07f32d3
hoon2hooni <sanghoon.engineer@gmail.com>
2023-08-17 16:25:33 +0900

    :tada: 8-4 시작전

---
+++ chapter-08/src/04-move-statements-to-callers/index.ts
@@ -0,0 +1,41 @@
+/**
+ * 호출자가 둘뿐인 상황
+ */
+
+interface OutStream {
+  write: (str: string) => void;
+}
+
+export type Person = { name: string; photo: Photo };
+export type Photo = {
+  title: string;
+  date: Date;
+  location: {
+    name: string;
+  };
+};
+
+declare function recentDateCutoff(): Date;
+declare function renderPhoto(outStream: OutStream, photo: Photo): void;
+
+export function renderPerson(outStream: OutStream, person: Person) {
+  outStream.write(`<p>${person.name}</p>\n`);
+  renderPhoto(outStream, person.photo);
+  emitPhotoData(outStream, person.photo);
+}
+
+function listRecentPhotos(outStream: OutStream, photos: Photo[]) {
+  photos
+    .filter((p) => p.date > recentDateCutoff())
+    .forEach((p) => {
+      outStream.write('<div>\n');
+      emitPhotoData(outStream, p);
+      outStream.write('</div>\n');
+    });
+}
+
+function emitPhotoData(outStream: OutStream, photo: Photo) {
+  outStream.write(`<p>제목: ${photo.title}</p>\n`);
+  outStream.write(`<p>날짜: ${photo.date.toDateString()}</p>\n`);
+  outStream.write(`<p>위치: ${photo.location}</p>\n`);
+}
