77f47e5673d78c283ddf140d9decf993c4f03eaa
hoon2hooni <sanghoon.engineer@gmail.com>
2023-08-17 16:28:50 +0900

    :sparkles: 8-4 리펙토링 후

--- chapter-08/src/04-move-statements-to-callers/index.ts
+++ chapter-08/src/04-move-statements-to-callers/index.ts
@@ -22,6 +22,7 @@ export function renderPerson(outStream: OutStream, person: Person) {
   outStream.write(`<p>${person.name}</p>\n`);
   renderPhoto(outStream, person.photo);
   emitPhotoData(outStream, person.photo);
+  outStream.write(`<p>위치: ${person.photo.location}</p>\n`);
 }
 
 function listRecentPhotos(outStream: OutStream, photos: Photo[]) {
@@ -30,6 +31,7 @@ function listRecentPhotos(outStream: OutStream, photos: Photo[]) {
     .forEach((p) => {
       outStream.write('<div>\n');
       emitPhotoData(outStream, p);
+      outStream.write(`<p>위치: ${p.location}</p>\n`);
       outStream.write('</div>\n');
     });
 }
@@ -37,5 +39,4 @@ function listRecentPhotos(outStream: OutStream, photos: Photo[]) {
 function emitPhotoData(outStream: OutStream, photo: Photo) {
   outStream.write(`<p>제목: ${photo.title}</p>\n`);
   outStream.write(`<p>날짜: ${photo.date.toDateString()}</p>\n`);
-  outStream.write(`<p>위치: ${photo.location}</p>\n`);
 }
