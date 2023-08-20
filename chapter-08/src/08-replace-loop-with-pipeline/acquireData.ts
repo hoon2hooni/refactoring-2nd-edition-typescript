31d75a4c05d333f0dc463bc8e30ff6eb936d12c7
hoon2hooni <sanghoon.engineer@gmail.com>
2023-08-21 08:24:50 +0900

    :sparkles: 8-8 리펙토링 후

--- chapter-08/src/08-replace-loop-with-pipeline/acquireData.ts
+++ chapter-08/src/08-replace-loop-with-pipeline/acquireData.ts
@@ -1,25 +1,9 @@
 function acquireData(input: string) {
-  const lines = input.split('\n');
-  let firstLine = true;
-  const result = [];
-
-  for (const line of lines) {
-    if (firstLine) {
-      firstLine = false;
-
-      continue;
-    }
-
-    if (line.trim() === '') {
-      continue;
-    }
-
-    const record = line.split(',');
-
-    if (record[1].trim() === 'India') {
-      result.push({ city: record[0].trim(), phone: record[2].trim() });
-    }
-  }
-
-  return result;
+  return input
+    .split('\n')
+    .slice(1) //
+    .filter((line) => line.trim() !== '')
+    .map((line) => line.split(','))
+    .filter((fields) => fields[1].trim() === 'India')
+    .map((fields) => ({ city: fields[0].trim(), phone: fields[2].trim() }));
 }
