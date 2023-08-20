4a6562510a6299f38e34403b6f18e810c94d3196
hoon2hooni <sanghoon.engineer@gmail.com>
2023-08-20 20:30:07 +0900

    :sparkles: 8-6 리펙토링 후

--- chapter-08/src/06-slide-statements/example-02.js
+++ chapter-08/src/06-slide-statements/example-02.js
@@ -2,17 +2,18 @@
  * 예시
  */
 
-const pricingPlan = retrievePricingPlan();
-const order = retrieveOrder();
 const baseCharge = pricingPlan.base;
-let charge;
 const chargePerUnit = pricingPlan.unit;
+const order = retrieveOrder();
 const units = order.units;
+let charge = baseCharge + units * chargePerUnit;
+
+const pricingPlan = retrievePricingPlan();
 let discount;
-charge = baseCharge + units * chargePerUnit;
 let discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0);
 if (order.isRepeat) {
   discount += 20;
 }
+
 charge = charge - discount;
 chargeOrder(charge);
--- chapter-08/src/06-slide-statements/example-03.js
+++ chapter-08/src/06-slide-statements/example-03.js
@@ -5,10 +5,9 @@ let result;
 
 if (availableResources.length === 0) {
   result = createResource();
-  allocatedResources.push(result);
 } else {
   result = availableResources.pop();
-  allocatedResources.push(result);
 }
+allocatedResources.push(result);
 
 return result;
