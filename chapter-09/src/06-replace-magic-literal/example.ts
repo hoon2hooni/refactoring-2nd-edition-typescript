761859795e078bf5090ecdbe8c08f6c1f7e5377a
hoon2hooni <sanghoon.engineer@gmail.com>
2023-08-23 19:50:33 +0900

    :tada: 9-6 리펙토링 후

--- chapter-09/src/06-replace-magic-literal/example.ts
+++ chapter-09/src/06-replace-magic-literal/example.ts
@@ -1,6 +1,9 @@
 /**
- * 리팩토링 전
+ * 리팩토링 후
  */
+
+const STANDARD_GRAVITY = 9.81;
+
 function potentialEnergy(mass: number, height: number) {
-  return mass * 9.81 * height;
+  return mass * STANDARD_GRAVITY * height;
 }
