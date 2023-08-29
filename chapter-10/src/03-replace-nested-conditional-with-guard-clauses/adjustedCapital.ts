c82e54c852dfe15a319c813b9445489b20d79298
hoon2hooni <sanghoon.engineer@gmail.com>
2023-08-30 00:48:48 +0900

    :recycle: 10-3 리펙터링 후

--- chapter-10/src/03-replace-nested-conditional-with-guard-clauses/adjustedCapital.ts
+++ chapter-10/src/03-replace-nested-conditional-with-guard-clauses/adjustedCapital.ts
@@ -7,14 +7,16 @@ type Instrument = {
 };
 
 function adjustedCapital(anInstrument: Instrument) {
-  let result = 0;
-  if (anInstrument.capital > 0) {
-    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-      result =
-        (anInstrument.income / anInstrument.duration) *
-        anInstrument.adjustmentFactor;
-    }
+  if (
+    anInstrument.capital <= 0 ||
+    anInstrument.interestRate <= 0 ||
+    anInstrument.duration <= 0
+  ) {
+    return 0;
   }
 
-  return result;
+  return (
+    (anInstrument.income / anInstrument.duration) *
+    anInstrument.adjustmentFactor
+  );
 }
--- chapter-10/src/03-replace-nested-conditional-with-guard-clauses/payAmount.ts
+++ chapter-10/src/03-replace-nested-conditional-with-guard-clauses/payAmount.ts
@@ -4,19 +4,14 @@ export type Employee = {
 };
 
 function payAmount(employee: Employee) {
-  let result;
-
   if (employee.isSeparated) {
-    result = { amount: 0, reasonCode: 'SEP' };
-  } else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: 'RET' };
-    } else {
-      result = someFinalComputation();
-    }
+    return { amount: 0, reasonCode: 'SEP' };
   }
 
-  return result;
+  if (employee.isRetired) {
+    return { amount: 0, reasonCode: 'RET' };
+  }
+  return someFinalComputation();
 }
 
 function someFinalComputation() {
