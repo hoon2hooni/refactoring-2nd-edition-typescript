4e2d8f1b08b6f9c21d58daaa0f01f0ce8b075256
hoon2hooni <sanghoon.engineer@gmail.com>
2023-09-11 23:42:30 +0900

    :tada: 11-06 리펙토링 전

--- chapter-11/src/06-replace-query-with-parameter/HeatingPlan.ts
+++ chapter-11/src/06-replace-query-with-parameter/HeatingPlan.ts
@@ -1,5 +1,26 @@
+declare const thermostat: {
+  selectedTemperature: number;
+  currentTemperature: number;
+};
+
+declare const thePlan: {
+  targetTemperature: number;
+};
+
+declare function setToHeat(): void;
+declare function setToCool(): void;
+declare function setOff(): void;
+
 class HeatingPlan {
-  get targetTemperature(aPlan) {
+  private _max: number;
+  private _min: number;
+
+  constructor(min: number, max: number) {
+    this._max = max;
+    this._min = min;
+  }
+
+  get targetTemperature() {
     if (thermostat.selectedTemperature > this._max) {
       return this._max;
     } else if (thermostat.selectedTemperature < this._min) {
