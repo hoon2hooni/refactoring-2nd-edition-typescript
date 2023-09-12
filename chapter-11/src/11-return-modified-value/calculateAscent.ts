6d9dd535bda7b0af5d57376ebb34bd0e5d32e0e9
hoon2hooni <sanghoon.engineer@gmail.com>
2023-09-12 18:09:31 +0900

    :tada: 11-11 리펙토링 전

---
+++ chapter-11/src/11-return-modified-value/calculateAscent.ts
@@ -0,0 +1,24 @@
+export interface Point {
+  elevation: number;
+}
+
+declare const points: Point[];
+declare function calculateTime(): number;
+declare function calculateDistance(): number;
+
+let totalAscent = 0;
+let totalTime = 0;
+let totalDistance = 0;
+calculateAscent();
+calculateDistance();
+
+const pace = totalTime / 60 / totalDistance;
+
+function calculateAscent() {
+    for (let i = 1; i < points.length; i++) {
+    const verticalChange = points[i].elevation - points[i - 1].elevation;
+    
+    totalAscent += verticalChange > 0 ? verticalChange : 0;
+  }
+  return totalAscent;
+}
