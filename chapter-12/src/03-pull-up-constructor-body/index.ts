e218f32b4247be74ce9fa1ece422bf32dedfb352
hoon2hooni <sanghoon.engineer@gmail.com>
2023-09-17 18:50:55 +0900

    :recycle: 12-3 리펙토링

--- chapter-12/src/03-pull-up-constructor-body/index.ts
+++ chapter-12/src/03-pull-up-constructor-body/index.ts
@@ -1,25 +1,24 @@
-class Party {}
+export class Party {
+  private _name: string;
+  constructor(name: string) {
+    this._name = name;
+  }
+}
 
 export class Employee extends Party {
-  private _name: string;
   private _id: string;
   private _monthlyCost: number;
   constructor(name: string, id: string, monthlyCost: number) {
-    super();
-
-    this._name = name;
+    super(name);
     this._id = id;
     this._monthlyCost = monthlyCost;
   }
 }
 
 export class Department extends Party {
-  private _name: string;
   private _staff: string;
   constructor(name: string, staff: string) {
-    super();
-
-    this._name = name;
+    super(name);
     this._staff = staff;
   }
 }
